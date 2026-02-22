27caf5c3f0312da75d1fdc67cc6c66139a146da9
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2026-02-22 23:46:51 +0400

    Fixed routing bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import { routes } from "./routes";
 import "./App.css";
 
